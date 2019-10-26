58a9dba5e2d5ee4e87ba7a0817dc300585122ee0
WhitePeachTea <penglei2123@gmail.com>
2019-10-26 10:09:17 -0400

    I am thinking create basic div through HTML rather than in JS file

--- Script.js
+++ Script.js
@@ -1,19 +1,20 @@
-
-var time = questions.length * 5
-var timerEl = document.getElementById("time");
-
 var QuizGame = function(){
+
+    var score = 0;
+    var Playername = ""; 
+    var time = questions.length * 5;
+    var timerContent = document.getElementById("time");
+    var timer;
+    var quizStatus = 0;
 
         function init(){
             // this task contains steps to _initialize the game_
             // ex. setting the score to zero
             // loading the questions
-            // etc.
-            var score = 0;
-            var Playername = ""; 
-            var timer =0;
+            // etc.            
             startGame();
         }
+
         
         function startGame(){
             // this task handles starting the game
@@ -28,21 +29,45 @@ var QuizGame = function(){
             startPage.appendChild(startButton);
             document.getElementById("div1").appendChild(startPage);
             document.getElementById('startquiz').innerHTML = "Start Quiz!";
-            document.getElementById("startquiz").addEventListener("click",startBtn_onClick(event));
+            var questionSection = document.createElement("div");
+            questionSection.setAttribute("id", "questions");
+            questionSection.setAttribute("class", "hide");
+            var title = document.createElement("h2");
+            title.setAttribute("id", "title");
+            var choicesSection = document.createElement("div");
+            choicesSection.setAttribute("id", "choices");
+            choicesSection.setAttribute("class", "choices");
+            generateQuestions();
         }
-        
+
+        function generateQuestions(event){
+            var quizStatus = 0;
+            var thisQuestion = questions[quizStatus];
+           document.getElementById("title").textContent = thisQuestion.title;
+            choicesSection.innerHTML = "";
+            thisQuestion.choices.forEach(function(choice, i) {
+            var choiceButton = document.createElement("button");
+            choiceButton.setAttribute("class", "choice");
+            choiceButton.setAttribute("value", choice);
+            choiceButton.textContent = i + 1 + ". " + choice;          
+            choiceButton.onclick = questionClick;
+            choicesSection.appendChild(choiceButton);
+        });
+            startBtn_onClick();
+        }
+            
         function startBtn_onClick(event){
             // this task handles what to do when you click on the start button
             // shows a new page (maybe) that
             // displays the questions
             // that is all that it does.
+            document.getElementById("startquiz").addEventListener("click",startBtn_onClick(event));
             var start1 = document.getElementById("start");
             start1.setAttribute("class", "hide");
             var quizContainer = document.createElement("div");
             quizContainer.setAttribute("id", "quizcontainer");
             document.getElementById("div1").appendChild(quizContainer);
-
-            gameLogic(); // now run the game logic itself
+            gameLogic(); 
         }
         
         function gameLogic(){
@@ -50,47 +75,46 @@ var QuizGame = function(){
             // starts the timer
             // listen for click events of all quiz question buttons.
             // 
-            timerId = setInterval(clockTick, 1000);
-            document.getElementById("choices").addEventListener("click",quizBtns_onClick(event));
+            timer = setInterval(clockTick, 1000);
+            timerContent.textContent = time;
+
+            document.getElementsByClassName("choices").addEventListener("click",quizBtns_onClick(event));
         }
         
         function quizBtns_onClick(event){
             // this task handles what to do when you click on a question's "answer button"
             // maybe call a function that determines if question is answered correctly (or not) 
-            determineIfQuestionIsRightOrWrong(question);
-            //move to next question
-        }
-        
-        function determineIfQuestionIsRightOrWrong(question){
-            // this task, given a question, determines if the answer is right or wrong
-            // if question is correct, change the score and add some points
-            // if question is incorrect, change the score and decrease points
-            if (document.getElementById("choices").onclick == answer) {
-                alert("right");
-                changeTheScore(10);
-            }
-            else {
-                alert("wrong");
-                changeTheScore(-5);
-            }
+            if (this.value !== questions[thisQuestion].answer) {
+                time -= 15;
+            
+                if (time < 0) {
+                  time = 0;
+                }
+                
+                timerContent.textContent = time;
+            
+                feedbackEl.textContent = "Wrong!";
+              } else {
+            
+                feedbackEl.textContent = "Correct!";}
+                quizStatus++;
 
-        }
-        
-        function changeTheScore(value){
-            // this task, given a value will increase -or- decrease the score.
-            // that is all it does.
-            return score += value;
+            if (quizStatus === questions.length) {
+                  endGame();
+            } else {
+                generateQuestions();
+                }
         }
         
         function endGame(){
             // steps required when the game is ended.
         setTimeout(function(){ $("#quizcontainer").add("<h1>Timeout!<h1>"); }, 75000);
 
-        
         }
         
         // ... add additional functions as needed. THIS IS NOT COMPLETE.
         
         init(); // begins initializing the game
     };
+
     QuizGame();
