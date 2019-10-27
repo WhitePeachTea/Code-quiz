66907464c4ee7e2ccbc37dac3bdcb3ca5058691c
WhitePeachTea <penglei2123@gmail.com>
2019-10-27 03:31:27 -0400

    finally fix the question container/timer

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 var score = 0;
 var Playername = ""; 
-var time = questions.length * 5;
+var time = questions.length * 15;
 var timerContent = document.getElementById("time");
 var timer;
 var quizStatus = 0;
@@ -9,70 +9,85 @@ var questionSection = document.getElementById("questions");
 var startPage = document.getElementById("start");
 var choicesSection = document.getElementById("choices")
 var quizStatus = 0;
-
+var thisQuestion = questions[quizStatus];
+var answers = document.getElementById("answers")
 
 
 function generateQuestions(){
-    var thisQuestion = questions[quizStatus];
-    titleSection.textContent = thisQuestion.title;
+    titleSection.textContent = questions[quizStatus].title;
     choicesSection.innerHTML = "";
-    thisQuestion.choices.forEach(function(choice, i) {
+    questions[quizStatus].choices.forEach(function(choice, i) {
     var choicesButton = document.createElement("button");
     choicesButton.setAttribute("class", "choices");
     choicesButton.setAttribute("value", choice);
-    choicesButton.textContent = i + 1 + ". " + choice;          
+    choicesButton.textContent = i + 1 + ". " + choice;         
     choicesSection.appendChild(choicesButton);
- });
+});
+
+$(".choices").click(function(){
+    // this task handles what to do when you click on a question's "answer button"
+    // maybe call a function that determines if question is answered correctly (or not) 
+    if (this.value !== questions[quizStatus].answer) {
+        time -= 15;
+    
+        if (time < 0) {
+          time = 0;
+        }
+        
+        timerContent.textContent = time;
+    
+        answers.textContent = "Wrong!";
+      } else {
+    
+        answers.textContent = "Correct!";}
+        
+}); 
+
+$(".choices").click(function(){
+
+    quizStatus++;
+    
+    if (quizStatus === questions.length) {
+          endGame();
+    } else {
+        generateQuestions();
+        }
+    });
+
 }
- 
+
+$(".choices").click(function(){
+
+quizStatus++;
+
+if (quizStatus === questions.length) {
+      endGame();
+} else {
+    generateQuestions();
+    }
+});
+
 
 
 $("#startquiz").click(function(){
     $(startPage).hide();
   });
 
-        document.getElementById("startquiz").addEventListener("click",function(){
-            startPage.setAttribute("class", "hide");
+function setTimer(){
+  time--;
+  timerContent.textContent = time;
+
+  if (time <= 0) {
+    endGame();
+  }
+}
+
+document.getElementById("startquiz").addEventListener("click",function(){
             questionSection.removeAttribute("class", "hide");
             generateQuestions();
-        });
-            
-
-    function gameLogic(){
-        // this task deals with the game logic.
-        // starts the timer
-        // listen for click events of all quiz question buttons.
-        // 
-        timer = setInterval(clockTick, 1000);
-        timerContent.textContent = time;
-
-        document.getElementsByClassName("choices").addEventListener("click",quizBtns_onClick(event));
-    }
-    
-    function quizBtns_onClick(event){
-        // this task handles what to do when you click on a question's "answer button"
-        // maybe call a function that determines if question is answered correctly (or not) 
-        if (this.value !== questions[thisQuestion].answer) {
-            time -= 15;
-        
-            if (time < 0) {
-              time = 0;
-            }
-            
+            timer = setInterval(setTimer, 1000);
             timerContent.textContent = time;
-        
-            feedbackEl.textContent = "Wrong!";
-          } else {
-        
-            feedbackEl.textContent = "Correct!";}
-            quizStatus++;
-
-        if (quizStatus === questions.length) {
-              endGame();
-        } else {
-            generateQuestions();
-            }
-    }
+        });            
     
     function endGame(){
         // steps required when the game is ended.
@@ -82,6 +97,5 @@ $("#startquiz").click(function(){
     
     // ... add additional functions as needed. THIS IS NOT COMPLETE.
     
-    init(); // begins initializing the game
         
       
